ce25d5dcc2604230ad59ca2a747263a0df456870
Walterdaniel <shj1103s@naver.com>
2022-10-08 16:12:39 +0900

    깃허브 커밋 연습겸 JS공부파일 업로드 생애 첫 깃 커밋

---
+++ nomad_JS/Momentum/app.js
@@ -0,0 +1,21 @@
+// How to make Variables
+// 1.
+// const is Immutable
+const a = 5;
+const b = 2;
+const myName = "nico";
+// 2.
+// let is Mutable
+let c = 5;
+let d = 2;
+let myName2 = "daniel";
+
+// send log() to console
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName)
+
+// let variables can be changed
+myName2 = "Seo hui je"
+console.log("hello your new name is " + myName2)
