185daa5f387b8d36f748f2251352269357cbe3bd
Walterdaniel <shj1103s@naver.com>
2022-10-17 15:21:17 +0900

    Update

--- nomad_JS/Momentum/app.js
+++ nomad_JS/Momentum/app.js
@@ -54,3 +54,64 @@ console.log(daysOfWeek, nonsense)
 
 // 이러한 리스트들은 인덱싱을 통해 값을 하나씩 불러올 수 있다.
 console.log(daysOfWeek[1])
+
+// Objects
+// 설명이 필요하지 않은 데이터 리스트들은 array로,
+// 설명이 필요한 정보가 담긴 데이터 리스트들은 object로!
+const playerName = "nico";
+const playerPoints = 121212;
+const playerHandsome = false;
+const palyerFat = "little bit";
+
+// 대신
+// const player = {
+//     name : "nico",
+//     points : 10,
+//     fat : true,
+// };
+// console.log(player);
+// console.log(player.name);
+// player.name
+// 이렇게 오브젝트로 표현해주면 꺼내오기 쉽다.
+// 오브젝트 안의 값을 바꾸고 싶다면
+// player.name = "Daniel";
+// player.points = player.points+10;
+// console.log(player)
+// 이런 식으로 값만 바꿔주면 끝!
+
+// Functions part one
+// 펑션은 클래스와 비슷한데 한번 만들어놓으면 계속 쓸 수 있는 기능조각(?)이다.
+function sayHello(){
+    console.log("Hello my name is C");
+}
+sayHello();
+
+// Functions part two
+// 펑션에서 특정 값을 바꿔서 내보낼려면(?)
+function sayHello2(nameOfPerson, age){
+    console.log("Hello my name is", nameOfPerson, "and I'm", age);
+}
+
+sayHello2("nico", 10);
+sayHello2("Daniel", 20);
+sayHello2("Sam", 25);
+
+// 계산기(?)
+function plus(a, b){
+    console.log(a + b);
+}
+plus(8, 60);
+function divide(a, b){
+    console.log(a / b);
+}
+divide(60, 8);
+
+const player = {
+    name: "nico",
+    sayHello: function (nameOfPerson) {
+        console.log("hello",nameOfPerson);
+    },
+};
+
+console.log(player.name)
+player.sayHello("nico");
